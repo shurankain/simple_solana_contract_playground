d8638c5716ed0259d5c04c05a28ee7b6e7bba788
Oleksandr_Husiev <ohusiev@icloud.com>
2025-02-26 22:06:37 +0100

    added counter logic

--- programs/simple_solana_contract_playground/src/lib.rs
+++ programs/simple_solana_contract_playground/src/lib.rs
@@ -7,10 +7,34 @@ pub mod simple_solana_contract_playground {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.counter = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+      let base_account = &mut ctx.accounts.base_account;
+        base_account.counter += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+  #[account(init, payer = user, space = 8 + 8)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+  pub counter: u64,
+}
