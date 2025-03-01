84cae387f49ac4a0b5b853347b09464a817fe86c
Oleksandr_Husiev <ohusiev@icloud.com>
2025-03-01 21:37:36 +0100

    cleanup and switched to devnet

--- tests/simple_solana_contract_playground.ts
+++ tests/simple_solana_contract_playground.ts
@@ -4,20 +4,18 @@ import { SimpleSolanaContractPlayground } from "../target/types/simple_solana_co
 import { Keypair, SystemProgram } from "@solana/web3.js";
 
 describe("simple_solana_contract_playground", () => {
-  // Настраиваем Anchor для работы с тестовым провайдером
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.SimpleSolanaContractPlayground as Program<SimpleSolanaContractPlayground>;
 
-  // Создаём новый Keypair для PDA
   const baseAccount = Keypair.generate();
 
   it("Initializes the counter", async () => {
     const tx = await program.methods
       .initialize()
       .accounts({
-        baseAccount: baseAccount.publicKey, // Передаём PDA
+        baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
       } as any)
@@ -25,8 +23,6 @@ describe("simple_solana_contract_playground", () => {
       .rpc();
 
     console.log("Transaction Signature (Initialize):", tx);
-
-    // Проверяем, что счётчик создан и равен 0
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
     console.log("Counter initialized with value:", account.counter.toString());
   });
@@ -35,13 +31,11 @@ describe("simple_solana_contract_playground", () => {
     const tx = await program.methods
       .increment()
       .accounts({
-        baseAccount: baseAccount.publicKey, // Передаём PDA
+        baseAccount: baseAccount.publicKey,
       })
       .rpc();
 
     console.log("Transaction Signature (Increment):", tx);
-
-    // Проверяем, что счётчик увеличился
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
     console.log("Counter incremented to:", account.counter.toString());
   });
